a060a39db2c7f749d184c9ef08968065f8e84116
Stefan Petrovic <stefan@spetrovic.com>
2016-12-19 12:53:59 -0500

    Test...

--- PacSquare/GameScene.swift
+++ PacSquare/GameScene.swift
@@ -1,4 +1,4 @@
-//
+
 //  GameScene.swift
 //  PacSquare
 //
@@ -12,13 +12,19 @@ class GameScene: SKScene {
     
     //add Koala
     let koala = SKSpriteNode(imageNamed:"koala")
+    var border = SKNode()
     
     //Label and variable to track score
     let scoreLabel = SKLabelNode(fontNamed: "Helvetica-Bold")
     var score = 0 //tracks current score
     
+    //Label for Game Over Scene
+    let gameOver1 = SKLabelNode(fontNamed: "Helvetica-Bold")
+    let gameOver2 = ("Game Over :(")
     
-    
+    //label for Win Scene
+    let Win1 = SKLabelNode(fontNamed: "Helvetica-Bold")
+    let Win2 = ("You Win :D")
     
     
     override func didMove(to view: SKView) {
@@ -34,11 +40,11 @@ class GameScene: SKScene {
         //position of koala
         koala.position = CGPoint(x: 500, y: 500)
         koala.size = CGSize(width: 90, height: 140)
-        
         addChild(koala)
+        
         spawnPellet()
-        spawnBarrier()
         spawnMonster()
+        
         //add heads up display to show the score
         scoreLabel.text = String(score)
         scoreLabel.fontColor = SKColor.yellow
@@ -46,7 +52,21 @@ class GameScene: SKScene {
         scoreLabel.zPosition = 150 //make sure that the HUD is not under other nodes
         scoreLabel.position = CGPoint(x: size.width - size.width / 8, y: size.height - size.height / 4)
         addChild(scoreLabel)
-        }
+        
+        
+        //add heads up display to show when you win the game
+        gameOver1.text = String(gameOver2)
+        gameOver1.fontColor = SKColor.yellow
+        gameOver1.fontSize = 96
+        gameOver1.zPosition = 150 //make sure that the HUD is not under other nodes
+        gameOver1.position = CGPoint(x: size.width / 2, y: size.height / 2)
+        
+        Win1.text = String(Win2)
+        Win1.fontColor = SKColor.yellow
+        Win1.fontSize = 96
+        Win1.zPosition = 150 //make sure that the HUD is not under other nodes
+        Win1.position = CGPoint(x: size.width / 2, y: size.height / 2)
+    }
     
     
     // This is a function that runs every tie SpriteKit updates the game frame
@@ -55,9 +75,9 @@ class GameScene: SKScene {
         checkCollisions()
         checkCollisions2()
         checkCollisions3()
+        checkCollisions4()
     }
     
-    //touch commands
     //touch commands
     
     //method responds to a single touch
@@ -70,280 +90,149 @@ class GameScene: SKScene {
         }
         let touchLocation = touch.location(in: self)
         
-        
-        //print location of touch out in console
-        print(touchLocation.x)
-        print(touchLocation.y)
-        
-        //koala lines of code
-        //koala lines of code
-        
         //make koala move horizontally
         let destination = CGPoint(x: touchLocation.x, y: touchLocation.y)
         
-        
-        
         //create action
-        let actionMove = SKAction.move(to: destination, duration: 1)
         
+        let actionMove = SKAction.move(to: destination, duration: 1)
         
         //tell koala to move
         koala.run(actionMove)
-        
-        //monster lines of code
-        //monster lines of code
-        
     }
-    let monster = SKSpriteNode(imageNamed:"monster")
-  
     
-    
-    
-    //pellet lines of code
-    
-    //spawning a single pellet
     func spawnPellet (){
         let pellet = SKSpriteNode(imageNamed: "pellet")
         //randomize horizontal position of the pellet
-        let horizontalPosition = CGFloat(arc4random_uniform(UInt32(size.width - 256 ))) + 128
+        let horizontalPosition = CGFloat(arc4random_uniform(UInt32(size.width - 300 ))) + 100
+        
+        let verticalPosition = CGFloat(arc4random_uniform(UInt32(size.height - 300))) + 100
         
         //randomize veritcal position of the pellet
-        let verticalPosition = CGFloat(arc4random_uniform(UInt32(size.height - 256))) + 128
-        
         let startingPosition = CGPoint(x: horizontalPosition, y: verticalPosition)
         pellet.position = startingPosition
         pellet.size = CGSize(width: 50, height: 50)
         pellet.name = "pellet"
         addChild(pellet)
-        
     }
-    
-    
-    
-    //barrier lines of code
-    
-    func spawnBarrier (){
+    func spawnMonster(){
+        let monster = SKSpriteNode(imageNamed: "monster")
+        //monster.setScale(1.0)
+        monster.size = CGSize (width:100, height: 100)
+        let startingPosition = CGPoint(x: size.width / 2, y: size.height / 2)
         
-        
-        // Left edge barriers
-        for column in 1...12{
-            let barrier = SKSpriteNode(imageNamed: "barrier")
-            barrier.anchorPoint = CGPoint(x: 0, y: 0)
-            
-            
-            let startingPosition = CGPoint(x: -25, y: 128 * column)
-            
-            barrier.position = startingPosition
-            barrier.size = CGSize(width: 128, height: 128)
-            barrier.setScale(2.0)
-            barrier.name = "barrier"
-            addChild(barrier)
-            
-        }
-        
-        // Left edge barriers
-        for column in 1...12{
-            let barrier = SKSpriteNode(imageNamed: "barrier")
-            barrier.anchorPoint = CGPoint(x: 0, y: 0)
-            
-            
-            let startingPosition = CGPoint(x: -25, y: 128 * column)
-            
-            barrier.position = startingPosition
-            barrier.size = CGSize(width: 128, height: 128)
-            barrier.setScale(2.0)
-            barrier.name = "barrier"
-            addChild(barrier)
-            
-            
-            
-        }
-        // right edge barriers
-        for column in 1...12{
-            let barrier2 = SKSpriteNode(imageNamed: "barrier")
-            barrier2.anchorPoint = CGPoint(x: 0, y: 0)
-            
-            
-            let startingPosition = CGPoint(x: 1890, y: 128 * column)
-            
-            barrier2.position = startingPosition
-            barrier2.size = CGSize(width: 128, height: 128)
-            barrier2.setScale(2.0)
-            barrier2.name = "barrier"
-            addChild(barrier2)
-            
-        }
-        
-        // top  edge barriers
-        for row in 1...17{
-            let barrier3 = SKSpriteNode(imageNamed: "barrier")
-            barrier3.anchorPoint = CGPoint(x: 0, y: 0)
-            
-            
-            let startingPosition = CGPoint(x: 105 * row - 10, y: 1153)
-            
-            barrier3.position = startingPosition
-            barrier3.size = CGSize(width: 128, height: 128)
-            barrier3.setScale(2.0)
-            barrier3.name = "barrier"
-            addChild(barrier3)
-            
-        }
-        
-        // bottom  edge barriers
-        for row in 1...17{
-            let barrier4 = SKSpriteNode(imageNamed: "barrier")
-            barrier4.anchorPoint = CGPoint(x: 0, y: 0)
-            
-            
-            let startingPosition = CGPoint(x: 105 * row - 10, y: 128)
-            
-            barrier4.position = startingPosition
-            barrier4.size = CGSize(width: 128, height: 128)
-            barrier4.setScale(2.0)
-            barrier4.name = "barrier"
-            addChild(barrier4)
-            
-        }
+        monster.position = startingPosition
+        //give the monster a name
+        monster.name = "monster"
+        addChild(monster)
     }
-    
-    
     // HITBOX LINES OF CODE
     //This function checks for collisions between Koala and Monster
     
     func checkCollisions() {
-        
         // create emety list (array) that will contain all obstacles colliding with Santa
         var hitMonsters : [SKSpriteNode] = []
         
         // Code to find all obstacles currently colliding with Koala
         enumerateChildNodes(withName: "monster", using: {
             node, _ in
-            
             // get a reference to current node that was found with name "monster"
-            
             let monster = node as! SKSpriteNode
-            
             // Check to see if the koala is intersecting with koala
             if monster.frame.insetBy(dx: 20, dy: 20).intersects(self.koala.frame.insetBy(dx: 40, dy: 40)) {
                 
                 // This monster intersects with koala
                 hitMonsters.append(monster)
-                
             }
         })
-        
-        //loop over all the obstacles collding with koala
+        //loop over all the obstacles colliding with koala
         for monster in hitMonsters {
             koalaHit(by: monster)
-            
         }
     }
     // this functions removes monster from game
     func koalaHit(by monster: SKSpriteNode) {
+        let pellet = SKSpriteNode (imageNamed: "pellet")
         
         // remove monster from scene
         koala.removeFromParent()
+        
+        addChild(gameOver1)
+        pellet.removeFromParent()
     }
+    
     func checkCollisions2() {
-        
-        // create emety list (array) that will contain all obstacles colliding with Santa
-        var hitPellet : [SKSpriteNode] = []
-        
-        // Code to find all obstacles currently colliding with Koala
+        let monster = SKSpriteNode (imageNamed: "monster")
+        //check if pellet collides with koala
         enumerateChildNodes(withName: "pellet", using: {
             node, _ in
             
-            // get a reference to current node that was found with name "monster"
-            
             let pellet = node as! SKSpriteNode
-            
-            
             
             // Check to see if the koala is intersecting with koala
             if pellet.frame.intersects(self.koala.frame) {
-                
-                
                 self.score += 1
                 self.scoreLabel.text = String(self.score)
                 // This monster intersects with koala
                 pellet.removeFromParent()
+                pellet.physicsBody = nil
+                pellet.removeAllActions()
                 self.spawnPellet()
                 
+                if self.score == 35 {
+                    self.addChild(self.Win1)
+                    self.koala.removeFromParent()
+                    pellet.removeFromParent()
+                    monster.removeFromParent()
+                }
+                
             }
         })
-        
-        
     }
-    func spawnMonster(){
-        
-        self.monster.setScale(2.7)
-        
-        
-        let startingPosition = CGPoint(x: self.size.width / 2, y: self.size.height / 2)
-        self.monster.position = startingPosition
-        //give the monster a name
-        self.monster.name = "monster"
-        self.addChild(self.monster)
-    }
+    
     func checkCollisions3() {
-        
-       
-        
-        // create emety list (array) that will contain all obstacles colliding with Santa
-        var hitBarrier : [SKSpriteNode] = []
-        
-        // Code to find all obstacles currently colliding with Koala
-        enumerateChildNodes(withName: "barrier", using: {
+        // Code to find if monster collides with frame
+        enumerateChildNodes(withName: "monster", using: {
             node, _ in
-            
             // get a reference to current node that was found with name "monster"
+            let monster = node as! SKSpriteNode
             
-            let barrier = node as! SKSpriteNode
-            
-            
-            
-            // Check to see if the koala is intersecting with koala
-            
-          
-            
+            // Check to see if the monster is intersecting with frame
             let horizontalPosition = CGFloat(arc4random_uniform(UInt32(self.size.width)))
-           
-            
-            
             let verticalPosition = CGFloat(arc4random_uniform(UInt32(self.size.height)))
-            
-            
-            
-            
             let endingPosition = CGPoint(x: horizontalPosition, y: verticalPosition)
+            let actionMove = SKAction.move(to: endingPosition, duration: 1)
             
-                let actionMove = SKAction.move(to: endingPosition, duration: 3)
+            monster.run(actionMove)
             
-            self.monster.run(actionMove)
-            if barrier.frame.intersects(self.monster.frame) {
-                print("2")
+            if self.frame.intersects(monster.frame) {
                 
-            
-                // This monster intersects with koala
                 let horizontalPosition = CGFloat(arc4random_uniform(UInt32(self.size.width)))
-                
                 let verticalPosition = CGFloat(arc4random_uniform(UInt32(self.size.height)))
-                
-                
-                
-                
                 let endingPosition = CGPoint(x: horizontalPosition, y: verticalPosition)
+                let actionMove = SKAction.move(to: endingPosition, duration: 1)
                 
-                let actionMove = SKAction.move(to: endingPosition, duration: 3)
-                self.monster.run(actionMove)
-                
-                
+                monster.run(actionMove)
             }
-            
         })
-        
-        
+    }
+    
+    func checkCollisions4() {
+        // Code to check if  pellet hits frame
+        enumerateChildNodes(withName: "pellet", using: {
+            node, _ in
+            // get a reference to current node that was found with name "monster"
+            let pellet = node as! SKSpriteNode
+            
+            if pellet.frame.intersects(self.border.frame) {
+                print("pellet hit the border")
+                pellet.removeFromParent()
+                pellet.physicsBody = nil
+                pellet.removeAllActions()
+                self.spawnPellet()
+            }
+        })
     }
     
 }
+
